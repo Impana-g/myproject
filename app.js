9a265415064d5e7c3c812d485bb696f558fa6d33
Impana-g <impana.948@gmail.com>
2025-12-13 13:19:13 +0530

    Initial commit: simple web project

--- app.js
+++ app.js
@@ -1,3 +1,34 @@
 function showMessage() {
-    document.getElementById("result").innerText = "Thanks for clicking the button!";
+  document.getElementById("result").innerText =
+    "Button clicked! JavaScript is working.";
 }
+
+function sayHello() {
+  document.getElementById("result").innerText =
+    "Hello! Thanks for visiting my project.";
+}
+
+function greetUser() {
+  const name = document.getElementById("nameInput").value;
+
+  if (name === "") {
+    document.getElementById("result").innerText =
+      "Please enter your name!";
+  } else {
+    document.getElementById("result").innerText =
+      "Welcome, " + name + "! 👋";
+  }
+}
+
+function toggleDarkMode() {
+  document.body.classList.toggle("dark-mode");
+}
+
+function showDateTime() {
+  const now = new Date();
+  document.getElementById("dateTime").innerText =
+    "Current Date & Time: " + now.toLocaleString();
+}
+
+// Show date & time on page load
+showDateTime();
